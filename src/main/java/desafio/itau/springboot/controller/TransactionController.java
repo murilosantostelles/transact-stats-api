0bc0668c93cf307c3b1a835583e4c696ce95e704
Murilo Santos Telles <murilosantos.work@gmail.com>
2026-03-25 15:47:40 -0300

    feat: endpoint clearTransactions implementado

--- src/main/java/desafio/itau/springboot/controller/TransactionController.java
+++ src/main/java/desafio/itau/springboot/controller/TransactionController.java
@@ -6,10 +6,7 @@ import desafio.itau.springboot.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
 
@@ -27,10 +24,16 @@ public class TransactionController {
 
     @PostMapping
     public ResponseEntity<Void> createTransaction(@Valid @RequestBody TransactionDTO dto){
-        if(dto.getDataHora().isAfter(OffsetDateTime.now())){
+        if(dto.getDataHora().isAfter(OffsetDateTime.now()) || dto.getValor() <= 0){
             return ResponseEntity.unprocessableEntity().build();
         }
         transactionService.addTransaction(new Transaction(dto.getValor(), dto.getDataHora()));
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> clearTransactions(){
+        transactionService.clearTransactions();
+        return ResponseEntity.ok().build();
     }
 }

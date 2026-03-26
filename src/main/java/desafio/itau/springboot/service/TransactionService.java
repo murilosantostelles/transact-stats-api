47577672d34b26d24dbb66f50e67756321ce145a
Murilo Santos Telles <murilosantos.work@gmail.com>
2026-03-26 14:42:37 -0300

    feat: torna o tempo de cálculo de estatísitcas configurável

--- src/main/java/desafio/itau/springboot/service/TransactionService.java
+++ src/main/java/desafio/itau/springboot/service/TransactionService.java
@@ -2,7 +2,7 @@ package desafio.itau.springboot.service;
 
 import desafio.itau.springboot.entity.Transaction;
 import org.springframework.stereotype.Service;
-
+import org.springframework.beans.factory.annotation.Value;
 import java.time.OffsetDateTime;
 import java.util.DoubleSummaryStatistics;
 import java.util.Queue;
@@ -10,6 +10,9 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 
 @Service
 public class TransactionService {
+
+    @Value("${estatistica.tempo.segundos}")
+    private long segundos;
 
     private final Queue<Transaction> transactions = new ConcurrentLinkedDeque<>();
 
@@ -25,7 +28,7 @@ public class TransactionService {
         OffsetDateTime now = OffsetDateTime.now();
         return transactions.stream()
                 .filter(t -> t.getDataHora()
-                        .isAfter(now.minusSeconds(60)))
+                        .isAfter(now.minusSeconds(segundos)))
                 .mapToDouble(Transaction::getValor)
                 .summaryStatistics();
     }
